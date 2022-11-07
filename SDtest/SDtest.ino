02b969d56396299e3d237c19d4d3857a01e33470
jongbeom <ljb9906@naver.com>
2022-11-08 01:39:17 +0900

    Successful reading and printing files' name in SD card

--- SDtest/SDtest.ino
+++ SDtest/SDtest.ino
@@ -3,17 +3,19 @@
 #include <LiquidCrystal.h>
 #define PhaseA 18
 #define PhaseB 19
+
 LiquidCrystal display(12, 11, 5, 4, 3, 2);
 File root;
-File entry;
+File entry[16]; //모든 파일들을 담는 파일 배열 생성
+//개발 과정 중 파일명만 따로 뽑아서 출력하고, 한 개의 entry 클래스로
+//cnt 값에 맞는 파일을 열어 entry 클래스에 넣고 파일명을 저장하는 배열에 저장하려 했음.
+//하지만 이러한 방식은 파일 클래스가 제대로 저장되지 않고, 모든 배열에 파일 이름을 넣는 순간
+//맨 마지막 파일명으로 배열의 모든 요소들이 자동으로 초기화되는 문제가 발생.
+//따라서 모든 파일들을 각각의 클래스 배열에 넣어줘 파일들을 저장해서 cnt에 맞는 파일들을 그때그때 열 수 있고, 파일명도 성공적으로 출력할 수 있었음.
+
 
 const int chipSelect = 53;
-void file_name2buffer(File dir, int a);
-void print_title(int a);
-char filename[17][12];
 char state[2][8] = { "Stopped", "Playing" }; //재생상태 문자열을 담고있는 배열
-
-char* foo[17];
 
 
 int i = 0;
@@ -22,6 +24,8 @@ volatile int cnt = 0;
 volatile int play_state = 0;
 void ext_int3_init();
 void ext_int1_init();
+void readfile(File dir);
+
 
 void setup() {
    Serial.begin(115200);
@@ -35,57 +39,30 @@ void setup() {
    sei(); //golbal interrupt enable 시작
 
    SD.begin(chipSelect);
-
    root = SD.open("/"); //파일 경로해당하는 파일을 연다
-   file_name2buffer(root, 16);
-   Serial.println("--------------");
-   for (int i = 0; i < 17; i++) Serial.println(*filename[i]);
-   //entry = root.openNextFile();
-   //*foo[0] = entry.name();
-   //file_name2buffer(root);
+   readfile(root);
+   for (int i = 0; i < 16; +i++) {
+       Serial.print(entry[i].position()); //위치 출력용 코드
+       Serial.println(entry[i].name());
+   }
 }
 
 void loop() {
     display.setCursor(0, 0); //0번줄 0번째 칸에 커서 위치
     display.print("                "); //0번줄 전체 클리어
     display.setCursor(0, 0); //0번줄 0번째 칸에 커서 위치
-    
-    //file_name2buffer(root, 0);
-    
-    //display.print(print_title(1)); // 노래 제목 출력 구현 테스트, 나중에 제목 출력하는 함수로 다 묶어버리기
-    
+    display.print(entry[cnt].name()); //각 파일 인스턴스의 이름 출력
+
     display.setCursor(0, 1); //1번줄 0번째 칸에 커서 위치
     display.print("                "); //1번줄 전체 클리어
     display.setCursor(0, 1); //0번줄 0번째 칸에 커서 위치
     display.print(state[play_state]); //재생상태 출력 구현 테스트, 나중에 재생상태 표시하는 함수로 다 묶어버리기
+    /*파일 재생하는 함수 구현 필요!!!*/
 
     display.setCursor(0, 0);
     //Serial.println(print_title(1));
     delay(20); //display 유지를 위한 50ms동안의 delay.    
     
-}
-
-void file_name2buffer(File dir, int a) {
-    
-    
-    while (i<=a) {
-
-        File entry = dir.openNextFile();
-
-        //if (!entry) break; //다음 파일이 없으면 끝난다.
-
-        filename[i] = (char)(entry.name()); //현재 객체에 저장되어있는 파일의 이름 저장한다
-        Serial.println(*filename[i]);
-        display.setCursor(0, 0);
-        display.print(*filename[i]);
-        delay(500);
-        i++;
-        //display.setCursor(0, 0);
-        //display.print(*filename[0]);
-        //delay(200);
-    }
-    Serial.println("------------------");
-    for (int i = 0; i <= a; i++) Serial.println(*filename[i]);
 }
 
 void ext_int3_init() {
@@ -104,30 +81,25 @@ void ext_int1_init() {
     EIFR |= _BV(INTF1);
 }
 
+void readfile(File dir) {
+    dir.rewindDirectory();
+    dir.openNextFile();
+    for (int i = 0; i < 16; i++) {
+        entry[i] = dir.openNextFile();
+    }
+}
+
 ISR(INT1_vect) {
     if (play_state == 0) play_state = 1;
     else play_state = 0;
-}
+}  
 
 ISR(INT3_vect) {
     if (digitalRead(PhaseB)) cnt++;
 
     else cnt--;
 
-    if (cnt >= 17) cnt = 0;
+    if (cnt >= 15) cnt = 0;
 
-    else if (cnt <= 0) cnt = 1;
-}
-
-void print_title(int a) {
-    root.rewindDirectory();
-    File title = root.openNextFile();
-    for (int j = 0; j < a; j++) {
-        title = root.openNextFile();
-        //Serial.println(entry.name());
-    }
-    return title.name();
-    //display.print(entry.name());
-    //*foo[a] = entry.name();
-    //Serial.println(*foo[a]);
+    else if (cnt <= 0) cnt = 0; //0번째 인스턴스는 SYSTEM 폴더이므로 처음에만 출력하고 나중에는 생략 이 부분 추가코드로 수정 필요
 }
--- Test/Test.ino
+++ Test/Test.ino
@@ -1,18 +1,45 @@
-char *filename[5][6];
-String *test[5][6] = { "test1", "test2", "test3", "test4", "test5" };
-void setup() {
-	Serial.begin(115200);
+#include <SPI.h>
+#include <SD.h>
+#include <LiquidCrystal.h>
+File root;
+File test;
+File entry[16];
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int chipSelect = 53;
 
-	for (int i = 0; i < 5; i++) {
-		*filename[i] = *test[i];
+void readfile(File dir);
+
+void setup() {
+	lcd.begin(16, 2);
+	Serial.begin(115200);
+	
+	SD.begin(chipSelect);
+	root = SD.open("/");
+	//entry[0] = root.openNextFile();
+	//entry[1] = root.openNextFile();
+	//test = root.openNextFile();
+
+	readfile(root);
+
+	Serial.println("test start");
+	for (int i; i < 16; i++) {
+		Serial.println(entry[i].name());
 	}
-	Serial.println("--------------");
-	for (int i; i < 5; i++) {
-		Serial.println(filename[i]);
-		Serial.println(test[i]);
-	}
+	//Serial.println(test.name());
 }
 
 void loop(){
-
+	for (int i = 0; i < 16; i++) {
+		lcd.setCursor(0, 0);
+		lcd.print(entry[i].name());
+		delay(20);
+	}
 }
+
+void readfile(File dir) {
+	dir.rewindDirectory();
+
+	for (int i = 0; i < 16; i++) {
+		entry[i] = dir.openNextFile();
+	}
+} asdfsadf
